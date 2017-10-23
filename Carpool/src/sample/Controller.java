b2af48eca927395d9239cb7534d568c4b1c1b921
Rujiravee Yiwha <yiwha-yt13@hotmail.com>
2017-10-23 12:29:51 +0700

    Edit from Ong to UI

--- Carpool/src/sample/Controller.java
+++ Carpool/src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+
+
 public class Controller {
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private JFXTextField enterUsername;
+
+    @FXML
+    private JFXTextField enterPassword;
+
+    @FXML
+    void handle(ActionEvent event) {
+        System.out.println("test");
+        Main.callStage.setScene(new Scene(Main.feed, 900, 600));
+    }
+
 }
+
