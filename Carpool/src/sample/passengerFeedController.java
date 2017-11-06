1966ad78c10c0829030c9b6c283c9d841015a804
Rujiravee Yiwha <yiwha-yt13@hotmail.com>
2017-11-06 23:10:42 +0700

    add check place UI

--- Carpool/src/sample/passengerFeedController.java
+++ Carpool/src/sample/passengerFeedController.java
@@ -4,6 +4,7 @@ import CarpoolDB.Database;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -134,12 +135,13 @@ public class passengerFeedController {
     @FXML
     private JFXButton refresh;
 
+
     @FXML
     void refresh(ActionEvent event) { //to refresh the feed
         EntityManager em = Database.getConnection().createEntityManager();
         TypedQuery<Integer> check = em.createQuery("SELECT EventID FROM Event", Integer.class); //the first event of table Event is dummy event
         int j = 0; //check empty event
-        for ( int i : check.getResultList()) {
+        for (int i : check.getResultList()) {
             j = i;
         }
         System.out.println("Amount of event: " + j);
@@ -149,11 +151,10 @@ public class passengerFeedController {
             blockPane2.setVisible(false);
             blockPane3.setVisible(false);
             blockPane4.setVisible(false);
-        }
-        else if (j > 1){ // event is not empty (ev.getEventID = 1 = dummy)
+        } else if (j > 1) { // event is not empty (ev.getEventID = 1 = dummy)
             TypedQuery<Event> fromTable = em.createQuery("SELECT e FROM Event e WHERE EventID <> 1", Event.class);
 
-            for ( Event ev : fromTable.getResultList()) {
+            for (Event ev : fromTable.getResultList()) {
 
                 if (ev.getEventID() == 2) {
                     blockPane1.setVisible(true);
@@ -163,8 +164,21 @@ public class passengerFeedController {
                     date1.setText(ev.getDate());
                     seat1.setText(String.valueOf(ev.getSeatLeft()));
                     car1.setText(ev.getCarType());
-                }
-                else if (ev.getEventID() == 3) {
+
+                    // img check
+                    switch (ev.getTo()) {
+                        case "RNP": img1.setImage(new Image("place/RNP.png"));
+//                      case "Kae Ki": img1.setImage(new Image("place/RNP.png")); there is no kae ki picture
+                        case "E12 Building"  : img1.setImage(new Image("place/E12.png"));
+                        case "A Cafeteria" : img1.setImage(new Image("place/A.png"));
+                        case "Central Library" : img1.setImage(new Image("library/RNP.png"));
+                        case "Faculty of Science" : img1.setImage(new Image("place/sci.png"));
+                        case "Prathep Building" : img1.setImage(new Image("place/prathep.png"));
+                        case "ECC Building" : img1.setImage(new Image("place/ECC.png"));
+                    }
+
+
+                } else if (ev.getEventID() == 3) {
                     blockPane2.setVisible(true);
                     to2.setText(ev.getTo());
                     from2.setText(ev.getFrom());
@@ -172,8 +186,20 @@ public class passengerFeedController {
                     date2.setText(ev.getDate());
                     seat2.setText(String.valueOf(ev.getSeatLeft()));
                     car2.setText(ev.getCarType());
-                }
-                else if (ev.getEventID() == 4) {
+
+                    // img check
+                    switch (ev.getTo()) {
+                        case "RNP": img1.setImage(new Image("place/RNP.png"));
+//                      case "Kae Ki": img1.setImage(new Image("place/RNP.png")); there is no kae ki picture
+                        case "E12 Building"  : img1.setImage(new Image("place/E12.png"));
+                        case "A Cafeteria" : img1.setImage(new Image("place/A.png"));
+                        case "Central Library" : img1.setImage(new Image("library/RNP.png"));
+                        case "Faculty of Science" : img1.setImage(new Image("place/sci.png"));
+                        case "Prathep Building" : img1.setImage(new Image("place/prathep.png"));
+                        case "ECC Building" : img1.setImage(new Image("place/ECC.png"));
+                    }
+
+                } else if (ev.getEventID() == 4) {
                     blockPane3.setVisible(true);
                     to3.setText(ev.getTo());
                     from3.setText(ev.getFrom());
@@ -181,8 +207,20 @@ public class passengerFeedController {
                     date3.setText(ev.getDate());
                     seat3.setText(String.valueOf(ev.getSeatLeft()));
                     car3.setText(ev.getCarType());
-                }
-                else if (ev.getEventID() == 5) {
+
+                    // img check
+                    switch (ev.getTo()) {
+                        case "RNP": img1.setImage(new Image("place/RNP.png"));
+//                      case "Kae Ki": img1.setImage(new Image("place/RNP.png")); there is no kae ki picture
+                        case "E12 Building"  : img1.setImage(new Image("place/E12.png"));
+                        case "A Cafeteria" : img1.setImage(new Image("place/A.png"));
+                        case "Central Library" : img1.setImage(new Image("library/RNP.png"));
+                        case "Faculty of Science" : img1.setImage(new Image("place/sci.png"));
+                        case "Prathep Building" : img1.setImage(new Image("place/prathep.png"));
+                        case "ECC Building" : img1.setImage(new Image("place/ECC.png"));
+                    }
+
+                } else if (ev.getEventID() == 5) {
                     blockPane4.setVisible(true);
                     to4.setText(ev.getTo());
                     from4.setText(ev.getFrom());
@@ -190,6 +228,18 @@ public class passengerFeedController {
                     date4.setText(ev.getDate());
                     seat4.setText(String.valueOf(ev.getSeatLeft()));
                     car4.setText(ev.getCarType());
+
+                    // img check
+                    switch (ev.getTo()) {
+                        case "RNP": img1.setImage(new Image("place/RNP.png"));
+//                      case "Kae Ki": img1.setImage(new Image("place/RNP.png")); there is no kae ki picture
+                        case "E12 Building"  : img1.setImage(new Image("place/E12.png"));
+                        case "A Cafeteria" : img1.setImage(new Image("place/A.png"));
+                        case "Central Library" : img1.setImage(new Image("library/RNP.png"));
+                        case "Faculty of Science" : img1.setImage(new Image("place/sci.png"));
+                        case "Prathep Building" : img1.setImage(new Image("place/prathep.png"));
+                        case "ECC Building" : img1.setImage(new Image("place/ECC.png"));
+                    }
                 }
             }
         }
