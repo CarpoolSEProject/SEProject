44e044feddfee4cc467c48d62c87d56b53bf3a2c
Rujiravee Yiwha <yiwha-yt13@hotmail.com>
2017-11-09 00:50:38 +0700

    map location waiting array

--- Carpool/src/EventDetail/detailController.java
+++ Carpool/src/EventDetail/detailController.java
@@ -123,16 +123,6 @@ public class detailController {
         this.seat.setVisible(true);
         this.car.setVisible(true);
 
-        //        locationWaiting[0] = "wait_KaeKi";
-//        locationWaiting[1] = "wait_E12";
-//        locationWaiting[2] = "wait_A";
-//        locationWaiting[3] = "wait_lib";
-//        locationWaiting[4] = "wait_sci";
-//        locationWaiting[5] = "wait_prathep";
-//        locationWaiting[6] = "wait_ecc";
-//        locationWaiting[7] = "wait_RNP";
-        //Guess DB return locationwaiting as an array na ja
-
 
         if (to == "E12 Building"){
             line_to.setFill(Color.valueOf("#1bb64f"));
@@ -218,6 +208,133 @@ public class detailController {
 //
 //
 //        }
+
+        //locationWaiting[0] = "wait_KaeKi";
+//        locationWaiting[1] = "wait_E12";
+//        locationWaiting[2] = "wait_A";
+//        locationWaiting[3] = "wait_lib";
+//        locationWaiting[4] = "wait_sci";
+//        locationWaiting[5] = "wait_prathep";
+//        locationWaiting[6] = "wait_ecc";
+//        locationWaiting[7] = "wait_RNP";
+        //Guess DB return locationwaiting as an array na ja
+        //Ex. locationWaiting = [1,0,1,0,0,0,0,0]
+
+
+        // count number of available location waiting
+        int countSize = 0;
+
+        for (int i = 0; i< 8; i++){
+            if (locationWaiting[i] == 1){
+                countSize++;
+                if (i == 0){
+                    choice1.setText("Kae Ki");
+                    choice2.setText("Kae Ki");
+                    choice3.setText("Kae Ki");
+                    choice4.setText("Kae Ki");
+                    choice5.setText("Kae Ki");
+
+                }
+                else if (i == 1){
+                    choice1.setText("E12 Building");
+                    choice2.setText("E12 Building");
+                    choice3.setText("E12 Building");
+                    choice4.setText("E12 Building");
+                    choice5.setText("E12 Building");
+                }
+                else if (i == 2){
+                    choice1.setText("A Cafeteria");
+                    choice2.setText("A Cafeteria");
+                    choice3.setText("A Cafeteria");
+                    choice4.setText("A Cafeteria");
+                    choice5.setText("A Cafeteria");
+                }
+                else if (i == 3){
+                    choice1.setText("Central Library");
+                    choice2.setText("Central Library");
+                    choice3.setText("Central Library");
+                    choice4.setText("Central Library");
+                    choice5.setText("Central Library");
+                }
+                else if (i == 4){
+                    choice1.setText("Faculty of Science");
+                    choice2.setText("Faculty of Science");
+                    choice3.setText("Faculty of Science");
+                    choice4.setText("Faculty of Science");
+                    choice5.setText("Faculty of Science");
+                }
+                else if (i == 5){
+                    choice1.setText("Prathep Building");
+                    choice2.setText("Prathep Building");
+                    choice3.setText("Prathep Building");
+                    choice4.setText("Prathep Building");
+                    choice5.setText("Prathep Building");
+                }
+                else if (i == 6){
+                    choice1.setText("ECC Building");
+                    choice2.setText("ECC Building");
+                    choice3.setText("ECC Building");
+                    choice4.setText("ECC Building");
+                    choice5.setText("ECC Building");
+                }
+                else {
+                    choice1.setText("RNP");
+                    choice2.setText("RNP");
+                    choice3.setText("RNP");
+                    choice4.setText("RNP");
+                    choice5.setText("RNP");
+                }
+
+            }
+        }
+
+        // set visible to choice of place to wait
+        if (countSize == 1){
+            choice1.setVisible(true);
+            line1.setVisible(true);
+        }
+        else if (countSize == 2){
+            choice1.setVisible(true);
+            choice2.setVisible(true);
+            line1.setVisible(true);
+            line2.setVisible(true);
+        }
+        else if (countSize == 3){
+            choice1.setVisible(true);
+            choice2.setVisible(true);
+            choice3.setVisible(true);
+            line1.setVisible(true);
+            line2.setVisible(true);
+            line3.setVisible(true);
+        }
+        else if (countSize == 4){
+            choice1.setVisible(true);
+            choice2.setVisible(true);
+            choice3.setVisible(true);
+            choice4.setVisible(true);
+            line1.setVisible(true);
+            line2.setVisible(true);
+            line3.setVisible(true);
+            line4.setVisible(true);
+        }
+        else if (countSize == 5){
+            choice1.setVisible(true);
+            choice2.setVisible(true);
+            choice3.setVisible(true);
+            choice4.setVisible(true);
+            choice5.setVisible(true);
+            line1.setVisible(true);
+            line2.setVisible(true);
+            line3.setVisible(true);
+            line4.setVisible(true);
+            line5.setVisible(true);
+        }
+
+
+
+
+
+
     }
 
 }
