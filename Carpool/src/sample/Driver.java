092c263bcd2b103845a95d324daf9c6749624b83
Rujiravee Yiwha <yiwha-yt13@hotmail.com>
2017-11-24 18:13:03 +0700

    Refactor: var name driver class

--- Carpool/src/sample/Driver.java
+++ Carpool/src/sample/Driver.java
@@ -7,26 +7,27 @@ public class Driver extends Person {
 
     private String carType;
     private String carLicense;
-    private String Rating;
+    private String rating;
     @OneToMany(cascade=CascadeType.ALL, mappedBy="Driver")
-    private Event DriverEvent;
+    private Event driverEvent;
 
 
-    public Driver(String Username, String Password, String Email, String Name, String Gender, Integer Age, String PhoneNo,String carType, String carLicense, String Rating, Event DriverEvent) {
-        super(Username, Password, Email, Name, Gender, Age, PhoneNo);
+    public Driver(String username, String password, String email, String name, String gender, Integer age, String phoneNo,
+                  String carType, String carLicense, String rating, Event driverEvent) {
+        super(username, password, email, name, gender, age, phoneNo);
         this.carType = carType;
         this.carLicense = carLicense;
-        this.Rating = Rating;
-        this.DriverEvent = DriverEvent;
+        this.rating = rating;
+        this.driverEvent = driverEvent;
     }
 
     public String getCarType() { return carType; }
 
     public String getCarLicense() { return carLicense; }
 
-    public String getRating() { return Rating; }
+    public String getRating() { return rating; }
 
-    public Event getDriverEvent() { return DriverEvent; }
+    public Event getDriverEvent() { return driverEvent; }
 
 
 }
