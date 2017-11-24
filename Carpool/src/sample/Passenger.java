4165bac917f2b5f97e5bad609e089eeac9367e85
Rujiravee Yiwha <yiwha-yt13@hotmail.com>
2017-11-24 19:01:07 +0700

    Refactor: delete unnecessary method

--- Carpool/src/sample/Passenger.java
+++ Carpool/src/sample/Passenger.java
@@ -6,17 +6,14 @@ import javax.persistence.*;
 public class Passenger extends Person {
 
     @OneToMany(cascade=CascadeType.ALL, mappedBy="Passenger")
-    public JoinEvent PassengerEvent;
+    public JoinEvent passengerEvent;
 
     public Passenger(String username, String password, String email, String name, String gender, Integer age, String phoneNo) {
         super(username, password, email, name, gender, age, phoneNo);
     }
 
     public JoinEvent getPassengerEvent() {
-        return PassengerEvent;
+        return passengerEvent;
     }
-
-    public void setPassengerEvent(JoinEvent passengerEvent) {
-        PassengerEvent = passengerEvent;
-    }
+    
 }
